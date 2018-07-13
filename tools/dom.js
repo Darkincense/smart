14953046ca88406a261fb6d51d5e24cfc5286b3c
xiaoyueyue165 <1656800216@qq.com>
2018-07-13 18:12:40 +0800

    update

--- tools/dom.js
+++ tools/dom.js
@@ -445,6 +445,33 @@ function prependHTML(el, html) {
   fragment = null;
 };
 
+// http://stackoverflow.com/a/35385518/1262580
+/**
+ * @param {String} HTML representing a single element
+ * @return {Element}
+ */
+function htmlToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+// var td = htmlToElement('<td>foo</td>'),
+//   div = htmlToElement('<div><span>nested</span> <span>stuff</span></div>');
+
+/**
+* @param {String} HTML representing any number of sibling elements
+* @return {NodeList} 
+*/
+function htmlToElements(html) {
+  var template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.childNodes;
+}
+
+// var rows = htmlToElements('<tr><td>foo</td></tr><tr><td>bar</td></tr>');
+
 
 //----------------------------------------- 事件相关------------------------------------------
 
