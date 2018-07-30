98fac9ccb67a225a03d2ade390ea3e19601d3064
xiaoyueyue165 <1656800216@qq.com>
2018-07-30 16:45:55 +0800

    vue——test

--- leetcode/初级算法/数组/3.旋转数组.js
+++ leetcode/初级算法/数组/3.旋转数组.js
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-
-<body>
-  <script>
     /**
      * @param {number[]} nums
      * @param {number} k
@@ -29,7 +18,3 @@
     console.log(nums1);
     console.log(nums2);
     console.log(nums3);
-  </script>
-</body>
-
-</html>
