fe925a2a8a70438ab1232681904adc88f984673d
xiaoyueyue165 <1656800216@qq.com>
2018-08-18 07:58:21 +0800

    fix:source-code-study

--- bulid/tools/ajax.js
+++ bulid/tools/ajax.js
@@ -1,51 +1,111 @@
-/*
- * Get an ajax content.
- *
- * @Use var diagram = $.parseJSON( getPostContent( '/doajax?action=login-status', data ) );
- */
-var getPostContent = function (url, data) {
-  var value = (function () {
-    var val = null;
-    $.ajax({
-      'type': 'POST',
-      'async': false,
-      'global': false,
-      'url': url,
-      'data': data,
-      'success': function (res) {
-        val = res;
-      }
-    });
-    return val;
-  })();
-  return value;
+//AJAX GET
+function getData(url, Func) {
+  $.ajax({
+    url: url,
+    type: 'GET',
+    cache: false,
+    dataType: 'json',
+    success: function (data) {
+      Func(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error('ajax(' + url + ')[' + jqXHR.status + ']' + jqXHR.statusText);
+      console.error(jqXHR.responseText);
+      console.error('[' + textStatus + ']' + errorThrown);
+    }
+  });
 }
 
-/*
- * Get an ajax content.
- *
- * @Use var diagram = $.parseJSON( getContent( '/doajax?action=login-status' ) );
- */
-var getContent = function (url) {
-  var value = (function () {
-    var val = null;
-    $.ajax({
-      'async': false,
-      'global': false,
-      'url': url,
-      'success': function (data) {
-        val = data;
-      }
-    });
-    return val;
-  })();
-  return value;
+// AJAX POST
+function loadData(url, param, Func) {
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: param,
+    dataType: 'json',
+    cache: false,
+    contentType: 'application/json; charset=UTF-8',
+    success: function (data) {
+      Func(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error('ajax(' + url + ')[' + jqXHR.status + ']' + jqXHR.statusText);
+      console.error(jqXHR.responseText);
+      console.error('[' + textStatus + ']' + errorThrown);
+    }
+  });
 }
-
-function encode(e) {
-  return function (t) {
-    return e + "::" + encodeURIComponent(t)
+//AJAX POST 
+function loadData2(url, param, Func) {
+  $.ajax({
+    url: url,
+    type: 'POST',
+    cache: false,
+    data: param,
+    dataType: 'json',
+    success: function (data) {
+      Func(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error('ajax(' + url + ')[' + jqXHR.status + ']' + jqXHR.statusText);
+      console.error(jqXHR.responseText);
+      console.error('[' + textStatus + ']' + errorThrown);
+    }
+  });
+}
+// ajax submit form
+function submitForm(url, param, Func, checkElement) {
+  var processing = $(checkElement).hasClass('X');
+  var rtoken = $('#rtoken').val();
+  // 页面存在rtoken添加在提交的参数内
+  if (rtoken != undefined && rtoken != null && rtoken != '') {
+    var obj = $.evalJSON(param);
+    obj.rtoken = rtoken;
+    param = $.toJSON(obj);
   }
+  if (!processing) {
+    $(checkElement).addClass('X');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: param,
+      cache: false,
+      dataType: 'json',
+      contentType: 'application/json; charset=UTF-8',
+      success: function (data) {
+        $(checkElement).removeClass('X');
+        Func(data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        $(checkElement).removeClass('X');
+        console.error('ajax(' + url + ')[' + jqXHR.status + ']' + jqXHR.statusText);
+        console.error(jqXHR.responseText);
+        console.error('[' + textStatus + ']' + errorThrown);
+      }
+    });
+  } else {
+    console.log('处理中请勿重复点击！');
+  }
+}
+
+//AJAX sync POST
+function syncLoadData(url, param, Func) {
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: param,
+    dataType: 'json',
+    async: false,
+    contentType: 'application/json; charset=UTF-8',
+    success: function (data) {
+      Func(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error('ajax(' + url + ')[' + jqXHR.status + ']' + jqXHR.statusText);
+      console.error(jqXHR.responseText);
+      console.error('[' + textStatus + ']' + errorThrown);
+    }
+  });
 }
 /**
  * @param  {setting}
