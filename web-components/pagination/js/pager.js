39433e14fabe4cd05a5777d1991844f5a7b67dfd
xiaoyueyue165 <1656800216@qq.com>
2018-08-29 10:30:47 +0800

    fix：web-components

--- web-components/pagination/js/pager.js
+++ web-components/pagination/js/pager.js
@@ -1,8 +1,35 @@
-class Pager {
-  constructor(options) {
-    let defaultOptions = {
+(function () {
+  var util = {
+    on: function (element, eventType, selector, fn) {
+      element.addEventListener(eventType, e => {
+        var el = e.target;
+        while (!el.matches(selector)) {
+          if (element === el) {
+            el = null;
+            break;
+          }
+          el = el.parentNode;
+        }
+        el && fn.call(el, e, el);
+      });
+      return element;
+    },
+    // http://stackoverflow.com/a/35385518/1262580
+    create: function (html, children) {
+      var template = document.createElement("template");
+      template.innerHTML = html.trim();
+      var node = template.content.firstChild;
+      if (children) {
+        dom.append(node, children);
+      }
+      return node;
+    }
+  };
+
+  function Pager(options) {
+    var defaultOptions = {
       element: null,
-      buttonCount: 10,
+      buttonCount: 5,
       currentPage: 1,
       totalPage: 1,
       pageQuery: '', // 'page'
@@ -13,109 +40,120 @@ class Pager {
         first: '<button class=first>首页</button>',
         last: '<button class=last>末页</button>',
       },
-    }
-    this.options = Object.assign({}, defaultOptions, options)
-    this.domRefs = {}
-    this.currentPage = parseInt(this.options.currentPage, 10) || 1
-    this.checkOptions().initHtml().bindEvents()
-  }
-  checkOptions() {
-    if (!this.options.element) {
-      throw new Error('element is required')
-    }
-    return this
+    };
+    this.options = Object.assign({}, defaultOptions, options);
+    this.domRefs = {};
+    this.currentPage = parseInt(this.options.currentPage, 10) || 1;
+    this.init();
   }
 
-
-  
-  bindEvents() {
-    dom.on(this.options.element, 'click', 'ol[data-role="pageNumbers"]>li', (e, el) => {
-      this.goToPage(parseInt(el.dataset.page, 10))
-    })
-    this.domRefs.first.addEventListener('click', () => {
-      this.goToPage(1)
-    })
-    this.domRefs.last.addEventListener('click', () => {
-      this.goToPage(this.options.totalPage)
-    })
-    this.domRefs.prev.addEventListener('click', () => {
-      this.goToPage(this.currentPage - 1)
-    })
-    this.domRefs.next.addEventListener('click', () => {
-      this.goToPage(this.currentPage + 1)
-    })
-  }
-
-  goToPage(page) {
-    if (!page || page > this.options.totalPage || page === this.currentPage) {
-      return
-    }
-    if (this.options.pageQuery) {
-      bom.queryString.set(this.options.pageQuery, page)
-    }
-    this.currentPage = page
-    this.options.element.dispatchEvent(new CustomEvent('pageChange', { detail: { page } }))
-    this.rerender()
-  }
-  rerender() {
-    this._checkButtons();
-    let newNumbers = this._createNumbers();
-    let oldNumbers = this.domRefs.numbers;
-    oldNumbers.parentNode.replaceChild(newNumbers, oldNumbers);
-    this.domRefs.numbers = newNumbers;
-  }
-  initHtml() {
-    let pager = (this.domRefs.pager = document.createElement('nav'))
-    this.domRefs.first = dom.create(this.options.templates.first)
-    this.domRefs.prev = dom.create(this.options.templates.prev)
-    this.domRefs.next = dom.create(this.options.templates.next)
-    this.domRefs.last = dom.create(this.options.templates.last)
-    this._checkButtons()
-    this.domRefs.numbers = this._createNumbers()
-    pager.appendChild(this.domRefs.first)
-    pager.appendChild(this.domRefs.prev)
-    pager.appendChild(this.domRefs.numbers)
-    pager.appendChild(this.domRefs.next)
-    pager.appendChild(this.domRefs.last)
-    this.options.element.appendChild(pager)
-    return this
-  }
-  _checkButtons() {
-    if (this.currentPage === 1) {
-      this.domRefs.first.setAttribute('disabled', '')
-      this.domRefs.prev.setAttribute('disabled', '')
-    } else {
-      this.domRefs.first.removeAttribute('disabled')
-      this.domRefs.prev.removeAttribute('disabled')
-    }
-    if (this.currentPage === this.options.totalPage) {
-      this.domRefs.next.setAttribute('disabled', '')
-      this.domRefs.last.setAttribute('disabled', '')
-    } else {
-      this.domRefs.next.removeAttribute('disabled')
-      this.domRefs.last.removeAttribute('disabled')
-    }
-  }
-  _createNumbers() {
-    let currentPage = this.currentPage
-    let { buttonCount, totalPage } = this.options
-    let start1 = Math.max(currentPage - Math.round(buttonCount / 2), 1)
-    let end1 = Math.min(start1 + buttonCount - 1, totalPage)
-    let end2 = Math.min(currentPage + Math.round(buttonCount / 2) - 1, totalPage)
-    let start2 = Math.max(end2 - buttonCount + 1, 1)
-    let start = Math.min(start1, start2)
-    let end = Math.max(end1, end2)
-
-    let ol = dom.create('<ol data-role="pageNumbers"></ol>')
-    let numbers = []
-    for (var i = start; i <= end; i++) {
-      let li = dom.create(`<li data-page="${i}">${this.options.templates.number.replace('%page%', i)}</li>`)
-      if (i === currentPage) {
-        li.classList.add('current')
+  Pager.prototype = {
+    constructor: Pager,
+    init: function () {
+      this.checkOptions().initHtml().bindEvents();
+    },
+    checkOptions: function () {
+      if (!this.options.element) {
+        throw new Error('element is required');
       }
-      ol.appendChild(li)
-    }
-    return ol
-  }
-}
+      return this;
+    },
+    bindEvents: function () {
+      util.on(this.options.element, 'click', 'ol[data-role="pageNumbers"]>li', (e, el) => {
+        this.goToPage(parseInt(el.dataset.page, 10))
+      });
+      this.domRefs.first.addEventListener('click', () => {
+        this.goToPage(1);
+      });
+      this.domRefs.last.addEventListener('click', () => {
+        this.goToPage(this.options.totalPage)
+      });
+      this.domRefs.prev.addEventListener('click', () => {
+        this.goToPage(this.currentPage - 1)
+      });
+      this.domRefs.next.addEventListener('click', () => {
+        this.goToPage(this.currentPage + 1)
+      });
+    },
 
+    goToPage: function (page) {
+      if (!page || page > this.options.totalPage || page === this.currentPage) {
+        return;
+      }
+      if (this.options.pageQuery) {
+        bom.queryString.set(this.options.pageQuery, page)
+      }
+      this.currentPage = page;
+      this.options.element.dispatchEvent(new CustomEvent('pageChange', {
+        detail: {
+          page
+        }
+      }));
+      this.rerender();
+    },
+    rerender: function () {
+      this._checkButtons();
+      var newNumbers = this._createNumbers();
+      var oldNumbers = this.domRefs.numbers;
+      oldNumbers.parentNode.replaceChild(newNumbers, oldNumbers);
+      this.domRefs.numbers = newNumbers;
+    },
+    initHtml: function () {
+      var pager = (this.domRefs.pager = document.createElement('nav'));
+      this.domRefs.first = dom.create(this.options.templates.first);
+      this.domRefs.prev = dom.create(this.options.templates.prev);
+      this.domRefs.next = dom.create(this.options.templates.next);
+      this.domRefs.last = dom.create(this.options.templates.last);
+      this._checkButtons();
+      this.domRefs.numbers = this._createNumbers();
+      pager.appendChild(this.domRefs.first);
+      pager.appendChild(this.domRefs.prev);
+      pager.appendChild(this.domRefs.numbers);
+      pager.appendChild(this.domRefs.next);
+      pager.appendChild(this.domRefs.last);
+      this.options.element.appendChild(pager);
+      return this;
+    },
+    _checkButtons: function () {
+      if (this.currentPage === 1) {
+        this.domRefs.first.setAttribute('disabled', '');
+        this.domRefs.prev.setAttribute('disabled', '');
+      } else {
+        this.domRefs.first.removeAttribute('disabled');
+        this.domRefs.prev.removeAttribute('disabled');
+      }
+      if (this.currentPage === this.options.totalPage) {
+        this.domRefs.next.setAttribute('disabled', '');
+        this.domRefs.last.setAttribute('disabled', '');
+      } else {
+        this.domRefs.next.removeAttribute('disabled');
+        this.domRefs.last.removeAttribute('disabled');
+      }
+    },
+    _createNumbers: function () {
+      var currentPage = this.currentPage;
+      var {
+        buttonCount,
+        totalPage
+      } = this.options;
+      var start1 = Math.max(currentPage - Math.round(buttonCount / 2), 1);
+      var end1 = Math.min(start1 + buttonCount - 1, totalPage);
+      var end2 = Math.min(currentPage + Math.round(buttonCount / 2) - 1, totalPage);
+      var start2 = Math.max(end2 - buttonCount + 1, 1);
+      var start = Math.min(start1, start2);
+      var end = Math.max(end1, end2);
+
+      var ol = util.create('<ol data-role="pageNumbers"></ol>');
+      var numbers = [];
+      for (var i = start; i <= end; i++) {
+        var li = util.create(`<li data-page="${i}">${this.options.templates.number.replace('%page%', i)}</li>`);
+        if (i === currentPage) {
+          li.classList.add('current');
+        }
+        ol.appendChild(li);
+      }
+      return ol;
+    }
+  };
+  window.Pager = Pager;
+})();
