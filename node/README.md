15f238f0a2b3fe691f8d7a6bdea60d20ee8b77dd
xiaoyueyue165 <1656800216@qq.com>
2018-08-31 21:13:41 +0800

    refactor

---
+++ node/README.md
@@ -0,0 +1,11 @@
+# Node-demos
+> 快速启动！
+
+## Guide
+
+- [rails365.net](https://www.rails365.net/playlists)
+- [project-nodejs-express-container](https://github.com/Microsoft/project-nodejs-express-container) -示例Nodejs Express应用程序
+
+## Demos
+
+ - [King-of-glory](https://github.com/xiaoyueyue165/King-of-glory) - 王者荣耀-nodejs英雄管理器（CRUD）
---
+++ node/upload-image/server/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var fs = require("fs");
+var app = express();
+var path = require("path");
+var upload = multer({ dest: './public/avatars/' });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post("/upload", upload.single('avatar'), function(req, res) {
+  console.log(req);
+  res.send(req.file);
+});
+
+var server = app.listen(8000, function() {
+  console.log("Listening on port %s...", server.address().port);
+});
