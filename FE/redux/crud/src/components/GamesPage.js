7c2aa05971f22706f4266927d1dafb48dba89988
xiaoyueyue165 <1656800216@qq.com>
2018-10-24 23:59:16 +0800

    crud start

---
+++ FE/redux/crud/src/components/GamesPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import GamesList from './GamesList';
+import { fetchGames } from '../actions';
+class GamesPage extends Component {
+  componentDidMount() {
+    this.props.fetchGames();
+  }
+  
+  render() {
+    return (
+      <div>
+       <GamesList games={ this.props.games } />
+      </div>
+    );
+  }
+}
+
+GamesPage.propTypes = {
+  games: PropTypes.array.isRequired
+}
+
+const mapStateToProps = (state)=> {
+  return {
+    games:state.games
+  }
+}
+
+export default connect(mapStateToProps, { fetchGames })(GamesPage);
--- FE/redux/crud/src/index.js
+++ FE/redux/crud/src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import GamesPage from './components/GamesPage';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(logger, thunk)
+  )
+);
+
+
+ReactDOM.render(
+  <Provider store={ store }>
+  <Router>
+      <div className="ui container">
+        <div className="ui three item menu">
+          <NavLink exact activeClassName="active" className="item" to="/">Home</NavLink>
+          <NavLink exact activeClassName="active" className="item" to="/games">Games</NavLink>
+          <NavLink activeClassName="active" className="item" to="/games/new">Add New Game</NavLink>
+        </div>
+        <Route exact path="/" component={ App } />
+        <Route exact path="/games" component={ GamesPage } />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
