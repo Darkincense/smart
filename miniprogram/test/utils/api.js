fb31c5708848f2e7bb8da11172b20f3baa7ec9be
xiaoyueyue165 <xiaoyueyue165@gmail.com>
2018-12-27 18:54:20 +0800

    svn init

--- miniprogram/test/utils/api.js
+++ miniprogram/test/utils/api.js
@@ -1,5 +1,5 @@
 var RootUrl = "http://anhancikong.test01.qcw100.com";
-var adUrl = RootUrl +"rest/2.0/banner/banner/List";
+var adUrl = RootUrl +"rest/2.0/banner/banner/List"; // 广告
 
 module.exports = {
   adUrl: adUrl
