d79235b445bee065273968a380e739dd3601622b
xiaoyueyue165 <1656800216@qq.com>
2019-04-06 15:04:22 +0800

    refactor

--- bulid/tools/index.js
+++ bulid/tools/index.js
@@ -10,6 +10,40 @@ function getType(a) {
 }
 
 /**
+ * 格式化时间戳
+ *
+ * @param {*} timestamp “1569859200”
+ * @param {*} hasHour true “2019-10-01”
+ * @returns
+ */
+function timestampToTime(timestamp, hasHour) {
+  var date = (function () {
+    if (timestamp.length === 10) {
+      return new Date(timestamp * 1000);
+    } else if (timestamp.length === 13) {
+      return new Date(Number(timestamp));
+    } else {
+      throw new Error("请检验传入的时间戳")
+    }
+  })(),
+    // 补 0
+    addZero = function (str) {
+      return Number(str) < 10 ? '0' + str : str
+    };
+  Y = date.getFullYear() + '-';
+  M = addZero(date.getMonth() + 1) + '-';
+  D = addZero(date.getDate());
+  h = date.getHours() + ':';
+  m = date.getMinutes() + ':';
+  s = date.getSeconds();
+
+  if (hasHour) {
+    return Y + M + D + h + m + s;
+  } else {
+    return Y + M + D;
+  }
+}
+/**
  * 四舍五入 格式化数字
  *
  * @param {*} number 8440.55
--- bulid/tools/utils/rem.js
+++ bulid/tools/utils/rem.js
@@ -1,6 +1,5 @@
 
 (function (doc, win) {
-
     var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         setRemResponse = function () {
             var vM = 750;
@@ -9,7 +8,6 @@
             var newfontSize = vfontSize * html.clientWidth / vM;
             html.style.fontSize = newfontSize + 'px';
         };
-
     doc.addEventListener('DOMContentLoaded', setRemResponse, false);
     win.addEventListener(resizeEvt, setRemResponse, false);
 })(document, window);
